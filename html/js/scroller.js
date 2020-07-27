fb2f0bb222ce5e787ebb54bd7a4433f060398e81
unknown <benjamin.jouanno@epitech.eu>
2020-07-27 17:44:41 +0200

    fix

--- html/js/scroller.js
+++ html/js/scroller.js
@@ -1,25 +1,15 @@
-$(function() {
+var snapScroll = $("header, section, footer").SnapScroll({
+    hashes: true
+});
 
-    if ($(window).width()>480){ 
-        var snapScroll = $("header, section, footer").SnapScroll({
-            hashes: true
-        });
+//Listen for active element change
+//You could listen globally like $(document)
+$("header").on(snapScroll.eventChangeActive, function (evt, newActive) {
+});
 
-        //Listen for active element change
-        //You could listen globally like $(document)
-        $("header").on(snapScroll.eventChangeActive, function (evt, newActive) {
-        });
-
-        //Listen for visible element change
-        //You could listen on a specific element like $("header")
-        $(document).on(snapScroll.eventChangeVisible, function (evt, visibleList) {
-        });
-    }
-  
-   else{
-
-   }
-  
-  });
+//Listen for visible element change
+//You could listen on a specific element like $("header")
+$(document).on(snapScroll.eventChangeVisible, function (evt, visibleList) {
+});
   
   
